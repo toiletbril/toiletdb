67d53a321a53a57fc25920fb23ce15fe8d683673
toiletbril <s9und@outlook.com>
2023-05-27 00:46:56 +0000

    Compress header

--- include/toiletdb.hpp
+++ include/toiletdb.hpp
@@ -63,7 +63,6 @@ template <typename T> void debug_puts(const T &s, const char *name);
 /**
  * @brief One bit in 32 bit integer that meeans either a type or a modifier.
  * A column can have only one type and any amount of modifiers.
- *
  * @see TDB_TYPE
  */
 enum ToiletType
@@ -82,8 +81,8 @@ enum ToiletType
 };
 
 /**
- * @brief Structure used in InMemoryParser to store information about
- * columns.
+ * @brief Structure used in InMemoryParser
+ * to store information about columns.
  */
 struct TableInfo
 {
@@ -175,16 +174,12 @@ public:
 struct FormatOne
 {
     static size_t read_version(std::fstream &file);
-
     static TableInfo read_types(std::fstream &file);
-
     static std::vector<Column *> deserealize(std::fstream &file,
                                              TableInfo &columns,
                                              std::vector<std::string> &names);
-
     static void write_header(std::fstream &file,
                              const std::vector<Column *> &data);
-
     static void serialize(std::fstream &file,
                           const std::vector<Column *> &data);
 };
@@ -195,24 +190,16 @@ private:
     const char *const &filename;
     size_t format_version;
     TableInfo columns;
-
     struct Private;
 
 public:
     InMemoryFileParser(const char *const &filename);
-
     ~InMemoryFileParser();
-
     size_t get_version() const;
-
     size_t get_id_column_index() const;
-
     bool exists() const;
-
     bool exists_or_create() const;
-
     std::vector<Column *> read_file();
-
     void write_file(const std::vector<Column *> &columns) const;
 };
 
@@ -234,66 +221,50 @@ public:
     /// @brief Opens up a file and loads up into memory.
     /// @warning Does not create a file. Will throw an error.
     InMemoryTable(const char *const &filename);
-
     ~InMemoryTable();
-
     const std::vector<Column *> &get_all() const;
-
     /// @brief Discards all changes made to in-memory vector, and reads file
     /// again.
     void reread_file();
-
     /// @brief Writes data stored in memory back to the file.
     void write_file() const;
-
     /// @brief Search in-memory vector by ID.
     /// O(log n)
     /// @return TDB_NOT_FOUND if element is not found.
     size_t search(const size_t &id) const;
-
     /// @brief Search in-memory vector by comparing values as std::string.
     /// O(n)
     /// @return TDB_NOT_FOUND if element is not found.
     std::vector<size_t> search(const std::string &name,
                                std::string &query) const;
-
     /// @brief Get one row from vector.
     /// One row means a value from each column.
     /// @warning You will need to get types and cast them yourself.
     /// @see get_column_types()
     /// @see get_column_type()
     std::vector<void *> get_row(const size_t &pos);
-
     /// @brief Adds row from
     /// One row means a value from each column.
     /// @warning You will need to get types and cast them yourself.
     /// @see get_column_types()
     /// @see get_column_type()
     int add(std::vector<std::string> &args);
-
     /// @brief Erases element with ID.
     bool erase_id(const size_t &id);
-
     /// @brief Clears all columns.
     void clear();
-
     size_t get_column_count() const;
-
     const std::vector<std::string> get_column_names() const;
-
     /// @brief Searches for column with specified name in a table.
     /// @returns Position of column in the table. TDB_NOT_FOUND if element
     /// is not found.
     size_t search_column_index(const std::string &name) const;
-
     /// @see ToiletType
     const std::vector<int> get_column_types() const;
-
     size_t total_rows() const;
-
     size_t get_next_id() const;
 };
 
 }; // namespace toiletdb
 
-#endif // TOILET_TYPES_H_
+#endif // TOILETDB_H_
