2145ad8f877ff61cfffeba78a7e936bcfc938c37
toiletbril <s9und@outlook.com>
2023-06-03 23:07:01 +0400

    fix py

--- scripts/make_db.py
+++ scripts/make_db.py
@@ -18,10 +18,10 @@ def group(suffixes: int) -> str:
     return choice(groups) + ((", " + ", ".join([ choice(groups) for _ in range(suffixes) ])) if suffixes else "")
 
 def generate_entry(suffixes: int) -> str:
-    return (f"|{randint(0, 99999999)}|{name(suffixes)}|{surname(suffixes)}|{group(0)}|{randint(10000000)}|\n")
+    return (f"|{randint(0, 99999999)}|{name(suffixes)}|{surname(suffixes)}|{group(0)}|{randint(10000000, 99999999)}|\n")
 
 def main():
-    if (len(argv) < 3):
+    if (len(argv) < 2):
         print("USAGE: python3 make_db.py <filename> [row count] [name complexity, number from 1 to 5+]")
         exit(1)
 
@@ -40,7 +40,7 @@ def main():
         for _ in range(count):
             f.write(generate_entry(suffixes))
 
-    print("Done.")
+    print(f"make_db.py: '{filename}' generated with {count} rows.")
 
 if __name__ == "__main__":
     main()
